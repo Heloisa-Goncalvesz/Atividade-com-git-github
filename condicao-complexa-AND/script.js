7f9db80efb958f712966876eb5e308cec950df4d
Heloisa-Goncalvesz <helo.goncalves0515@gmail.com>
2026-03-18 13:20:02 -0300

    Meu Primeiro Commit

---
+++ condicao-complexa-AND/script.js
@@ -0,0 +1,8 @@
+let idade = Nuber(prompt("Digite sua idade:"))
+let temCNH = prompt("Possui CNH? (sim ou nao)");
+
+if (idade >= 18 && tem CNH === "sim") {
+    alert("Pode dirigir"); 
+} else {
+        alert("Não pode dirigir");
+}
